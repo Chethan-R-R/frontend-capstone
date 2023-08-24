88445530ed111bfcbefeb680b0bae248b25dbbb4
Chethan-R-R <chethan9980rr@gmail.com>
2023-08-24 13:52:22 +0530

    added meta tags

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import logo from '../icons_assets/Logo .svg'
+export default function Header(){
+    return(
+        <header>
+            <img src={logo} alt="little lemon logo" />
+        </header>
+    )
+}
